f5ebb9151348a46df62411ce93ad19583cee658c
Lucas Albuquerque <lucastableless@gmail.com>
2021-07-25 20:32:08 -0300

    Add page component

---
+++ src/components/Page.tsx
@@ -0,0 +1,23 @@
+import React, { forwardRef, ReactNode } from "react";
+import { Helmet } from "react-helmet-async";
+import { Box, BoxProps } from "@material-ui/core";
+
+interface PageProps extends BoxProps {
+  children: ReactNode;
+  title?: string;
+}
+
+const Page = forwardRef<HTMLDivElement, PageProps>(({ children, title = "", ...other }, ref) => (
+  <Box ref={ref} {...other}>
+    <Helmet>
+      <title>
+        Oowlish Tracker -
+        {" "}
+        {title}
+      </title>
+    </Helmet>
+    {children}
+  </Box>
+));
+
+export default Page;
