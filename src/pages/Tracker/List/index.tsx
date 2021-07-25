0544d7f0b513103c64c32861f4305b19c5fd4f19
Lucas Albuquerque <lucastableless@gmail.com>
2021-07-25 20:32:25 -0300

    Add tracker list page

---
+++ src/pages/Tracker/List/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import Page from '../../../components/Page';
+
+const List: React.FC = () => {
+  return (
+    <Page title="Tracker List">
+       <Container>
+        <h1>hi</h1>
+       </Container>
+    </Page>
+  );
+}
+
+export default List;
