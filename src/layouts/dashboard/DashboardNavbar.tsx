f82c27832432b0d95cbd7cec6430931cba8d4759
Lucas Albuquerque <lucastableless@gmail.com>
2021-07-25 20:45:36 -0300

    Add navbar component

---
+++ src/layouts/dashboard/DashboardNavbar.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Toolbar from '@material-ui/core/Toolbar';
+import { AppBar } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+
+const drawerWidth = 240;
+
+const DashboardNavbar: React.FC = () => {
+
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  return (
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Responsive drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+  );
+}
+
+export default DashboardNavbar;
