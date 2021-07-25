35c75fbf528ca51a866bb965641f1d1b61aa23bc
Lucas Albuquerque <lucastableless@gmail.com>
2021-07-25 20:20:48 -0300

    Add routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,23 @@
+import React, { Suspense, lazy } from "react";
+import { useRoutes } from "react-router-dom";
+import DashboardLayout from "../layouts/dashboard";
+
+import { TRACKER_PATH } from "./paths";
+
+const Loadable = (Component: any) => (props: any) => (
+  <Suspense fallback={<h1>loading..</h1>}>
+    <Component {...props} />
+  </Suspense>
+);
+
+const TrackerList = Loadable(lazy(() => import("../pages/Dashboard")));
+
+export default function Router(): JSX.Element | null {
+  return useRoutes([
+    {
+      path: TRACKER_PATH,
+      element: <DashboardLayout />,
+      children: [{ path: "/", element: <TrackerList /> }],
+    },
+  ]);
+}
