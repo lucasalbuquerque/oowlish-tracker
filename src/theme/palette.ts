9e643e2c4e9a6e8a843aba619256a0ec5cbd7bb1
Lucas Albuquerque <lucastableless@gmail.com>
2021-07-25 19:53:47 -0300

    Add pallete

---
+++ src/theme/palette.ts
@@ -0,0 +1,154 @@
+import { alpha } from "@material-ui/core/styles";
+
+function createGradient(color1: string, color2: string): string {
+  return `linear-gradient(to bottom, ${color1}, ${color2})`;
+}
+
+interface GradientsPaletteOptions {
+  primary: string;
+  info: string;
+  success: string;
+  warning: string;
+  error: string;
+}
+
+declare module "@material-ui/core/styles/createPalette" {
+  interface TypeBackground {
+    neutral: string;
+  }
+  interface SimplePaletteColorOptions {
+    lighter: string;
+    darker: string;
+  }
+  interface PaletteColor {
+    lighter: string;
+    darker: string;
+  }
+  interface Palette {
+    gradients: GradientsPaletteOptions;
+  }
+  interface PaletteOptions {
+    gradients?: GradientsPaletteOptions;
+  }
+}
+
+declare module "@material-ui/core" {
+  interface Color {
+    0: string;
+    500_8: string;
+    500_12: string;
+    500_16: string;
+    500_24: string;
+    500_32: string;
+    500_48: string;
+    500_56: string;
+    500_80: string;
+  }
+}
+
+const PRIMARY = {
+  lighter: "#E3FAF5",
+  light: "#A7D6CF",
+  main: "#ffc21b",
+  dark: "#38736C",
+  darker: "#13352F",
+};
+const SECONDARY = {
+  lighter: "#D0D8E2",
+  light: "#94A5B7",
+  main: "#5C7189",
+  dark: "#333F4D",
+  darker: "#1D262F",
+};
+const INFO = {
+  lighter: "#B4E4D8",
+  light: "#83D2BF",
+  main: "#51C0A6",
+  dark: "#11A282",
+  darker: "#02573D",
+};
+const SUCCESS = {
+  lighter: "#C8E6C9",
+  light: "#81C784",
+  main: "#4CAF4F",
+  dark: "#388E3B",
+  darker: "#1B5E1F",
+};
+const WARNING = {
+  lighter: "#FFE0B2",
+  light: "#FFB84D",
+  main: "#FF9900",
+  dark: "#F57D00",
+  darker: "#E65200",
+};
+const ERROR = {
+  lighter: "#FFCDD2",
+  light: "#E57373",
+  main: "#F44336",
+  dark: "#D32F2F",
+  darker: "#B71B1C",
+};
+const GREY = {
+  100: "#F9FAFB",
+  200: "#F4F6F8",
+  300: "#DFE3E8",
+  400: "#C4CDD5",
+  500: "#919EAB",
+  600: "#637381",
+  700: "#454F5B",
+  800: "#212B36",
+  900: "#161C24",
+  500_8: alpha("#919EAB", 0.08),
+  500_12: alpha("#919EAB", 0.12),
+  500_16: alpha("#919EAB", 0.16),
+  500_24: alpha("#919EAB", 0.24),
+  500_32: alpha("#919EAB", 0.32),
+  500_48: alpha("#919EAB", 0.48),
+  500_56: alpha("#919EAB", 0.56),
+  500_80: alpha("#919EAB", 0.8),
+};
+const GRADIENTS = {
+  primary: createGradient(PRIMARY.light, PRIMARY.main),
+  info: createGradient(INFO.light, INFO.main),
+  success: createGradient(SUCCESS.light, SUCCESS.main),
+  warning: createGradient(WARNING.light, WARNING.main),
+  error: createGradient(ERROR.light, ERROR.main),
+};
+const COMMON = {
+  common: { black: "#000", white: "#fff" },
+  primary: { ...PRIMARY, contrastText: "#fff" },
+  secondary: { ...SECONDARY, contrastText: "#fff" },
+  info: { ...INFO, contrastText: "#fff" },
+  success: { ...SUCCESS, contrastText: GREY[800] },
+  warning: { ...WARNING, contrastText: GREY[800] },
+  error: { ...ERROR, contrastText: "#fff" },
+  grey: GREY,
+  gradients: GRADIENTS,
+  divider: GREY[500_24],
+  action: {
+    hover: GREY[500_8],
+    selected: GREY[500_16],
+    disabled: GREY[500_80],
+    disabledBackground: GREY[500_24],
+    focus: GREY[500_24],
+    hoverOpacity: 0.08,
+    disabledOpacity: 0.48,
+  },
+};
+
+const palette = {
+  light: {
+    ...COMMON,
+    text: { primary: GREY[800], secondary: GREY[600], disabled: GREY[500] },
+    background: { paper: "#fff", default: "#fff", neutral: GREY[200] },
+    action: { active: GREY[600], ...COMMON.action },
+  },
+  dark: {
+    ...COMMON,
+    text: { primary: "#fff", secondary: GREY[500], disabled: GREY[600] },
+    background: { paper: GREY[800], default: GREY[900], neutral: GREY[500_16] },
+    action: { active: GREY[500], ...COMMON.action },
+  },
+};
+
+export default palette;
