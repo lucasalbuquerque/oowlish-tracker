8720f0b3b59722abc9fc5eecb6165f54ed5935a9
Lucas Albuquerque <lucastableless@gmail.com>
2021-07-25 19:54:25 -0300

    Add theme config

---
+++ src/theme/index.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from "react";
+import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider, ThemeOptions, createTheme } from "@material-ui/core/styles";
+import StyledEngineProvider from "@material-ui/core/StyledEngineProvider";
+
+import palette from "./palette";
+import typography from "./typography";
+import GlobalStyles from "./globalStyles";
+
+type ThemeConfigProps = {
+  children: ReactNode;
+};
+
+export default function ThemeConfig({ children }: ThemeConfigProps): JSX.Element {
+  
+  const themeOptions: ThemeOptions = {
+    palette: { ...palette.light, mode: "light" },
+    typography,
+    direction: "ltr",
+  };
+
+  const theme = createTheme(themeOptions);
+
+  return (
+    <StyledEngineProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <GlobalStyles />
+        {children}
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
+}
