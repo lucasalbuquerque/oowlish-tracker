58e0de5a0c59f9f472f1fa288255ed8bea734a91
Lucas Albuquerque <lucastableless@gmail.com>
2021-07-26 01:02:34 -0300

    Implement react hook form

--- src/pages/Tracker/List/Form/index.tsx
+++ src/pages/Tracker/List/Form/index.tsx
@@ -1,11 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Stack from '@material-ui/core/Stack';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import { useForm } from "react-hook-form";
 
 const Form: React.FC = () => {
   
   const [showForm, setShowForm] = useState<Boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+  } = useForm({
+    mode: "all",
+  });
+
+   const onSubmit = useCallback((data: any) => {
+     console.log("dataa", data);
+   }, []);
 
   return (
     <>
@@ -15,23 +27,26 @@ const Form: React.FC = () => {
       </Stack>
       {showForm &&
       <Stack sx={{ paddingTop: 1, paddingBottom: 2 }}>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <Stack direction={{ xs: "column", sm: "row" }} spacing={{ xs: 3, sm: 2 }}>
             <TextField
                 id="description"
                 label="Description"
+                {...register("description")}
             />    
             <TextField
                 id="time"
                 label="Time entry"
                 type="time"
                 defaultValue="08:00"
+                {...register("startTime")}
             />
             <TextField
                 id="time"
                 label="End time"
                 type="time"
                 defaultValue="19:00"
+                {...register("endTime")}
             />
             <Button type="submit" variant="contained">Salvar</Button>
             </Stack>
