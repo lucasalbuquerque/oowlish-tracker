be2fc64ea994b29891da02d33e65f23e3e4764a6
Lucas Albuquerque <lucastableless@gmail.com>
2021-07-26 01:18:50 -0300

    Fix tracker schema

--- src/settings/yup/schemas/trackerSchema.ts
+++ src/settings/yup/schemas/trackerSchema.ts
@@ -3,8 +3,8 @@ import { mixed_required_field } from "../../../constants/validations";
 
 const trackerSchema = yup.object().shape({
   description: yup.string().required(mixed_required_field),
-  startTime: yup.date().required(mixed_required_field),
-  endTime: yup.date().required(mixed_required_field),
+  startTime: yup.string().required(mixed_required_field),
+  endTime: yup.string().required(mixed_required_field),
 });
 
 export interface TrackerSchema {
