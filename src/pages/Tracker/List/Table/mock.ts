80cb1ff7cc0df2106f8a37e2337550feb19c675e
Lucas Albuquerque <lucastableless@gmail.com>
2021-07-26 15:05:42 -0300

    Add tracker list mock

---
+++ src/pages/Tracker/List/Table/mock.ts
@@ -0,0 +1,30 @@
+export const trackerList = [
+    {
+        "id": 1,
+        "description": "Time entry",
+        "startTime": "08:00",
+        "endTime": "12:30",
+        "duration": "4:30"
+    },
+    {
+        "id": 2,
+        "description": "Exit time",
+        "startTime": "09:00",
+        "endTime": "20:30",
+        "duration": "11:30"
+    },
+    {
+        "id": 3,
+        "description": "Break Time",
+        "startTime": "10:00",
+        "endTime": "10:30",
+        "duration": "0:30"
+    },
+]
+
+export const columns = [
+    { id: 'description', label: 'Description' },
+    { id: 'startTime', label: 'Start Time' },
+    { id: 'endTime', label: 'End Time' },
+    { id: 'duration', label: 'Duration' }
+];
